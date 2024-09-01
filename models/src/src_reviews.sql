d0a543093702f846e1414ea010dd691eaff763b9
Hadiqa Malik <mhadiqa7998@gmail.com>
2024-09-01 14:23:52 +0500

    first commit

---
+++ models/src/src_reviews.sql
@@ -0,0 +1,16 @@
+-- Import Raw_Reviews
+
+WITH raw_reviews AS (
+ SELECT
+ *
+ FROM
+ AIRBNB.RAW.RAW_REVIEWS
+)
+SELECT
+ listing_id,
+ date AS review_date,
+ reviewer_name,
+ comments AS review_text,
+ sentiment AS review_sentiment
+FROM
+{{source('airbnb','reviews')}}
